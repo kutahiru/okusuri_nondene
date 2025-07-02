cb2c8db829b92c8200f94b1725a30cd2be240126
kuta <kuta0611@gmail.com>
2025-07-02 22:12:37 +0900

    テスト

--- app/jobs/line_notification_job.rb
+++ app/jobs/line_notification_job.rb
@@ -7,7 +7,7 @@ class LineNotificationJob < ApplicationJob
 
     medication_management = MedicationManagement.includes(:medication_schedule).find(notification_target.medication_management_id)
 
-    # 服薬済なら通知不要
+    # 服薬済なら通知不要a
     return if medication_management.medication_taken?
 
     if notification_target.medication_taker?
