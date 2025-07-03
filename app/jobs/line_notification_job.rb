25ef9ac2ef6ff91f4cf3aa880f0714ae5fbadc87
kuta <kuta0611@gmail.com>
2025-07-03 09:08:45 +0900

    LINE通知の不具合修正

--- app/jobs/line_notification_job.rb
+++ app/jobs/line_notification_job.rb
@@ -13,9 +13,8 @@ class LineNotificationJob < ApplicationJob
     if notification_target.medication_taker?
       # 服薬者の場合
       # リマインダー送信回数を加算する(異常時に何度もLINE通知送信を避けるため、送信回数加算が先)
-      if medication_management.increment(:reminder_sent_count)
-        LineNotificationService.send_line_message_with_button(medication_management.id, notification_target.uid, notification_target.group_name, notification_target.schedule_title)
-      end
+      medication_management.increment!(:reminder_sent_count)
+      LineNotificationService.send_line_message_with_button(medication_management.id, notification_target.uid, notification_target.group_name, notification_target.schedule_title)
     else
       # 見守り家族の場合
       # LINE通知送信
@@ -28,7 +27,7 @@ class LineNotificationJob < ApplicationJob
     # リマインド回数分通知済みなら通知不要
     return if medication_management.reminder_sent_count > medication_management.medication_schedule.reminder_count
 
-    medication_schedule = MedicationSchedule.find(notification_target.medication_schedule_id)
+    medication_schedule = medication_management.medication_schedule
     # リマインダー通知間隔を加算
     notification_target.medication_date_time = notification_target.medication_date_time + medication_schedule.reminder_interval.minutes
 
